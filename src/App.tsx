2780617a4febf1407455199464779a4b5ba3e5e1
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-11-13 13:33:44 +0500

    feat: all todos in card and convert into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import React, { FC, useState } from "react";
-import { InputField,BasicButton } from "./components";
+import React, { FC } from "react";
+import TodoApp from "./container";
 
-import { Row } from "antd";
+import { Row,Col } from "antd";
 import "antd/dist/antd.css";
 import "./App.css";
 
 const App: FC = () => {
-  interface Todos{
-    todo: string
-  }
-  const [todo, setTodo] = useState<string>('')
-  const [allTodos, setAllTodos] = useState<Todos[]>([])
-
-  const addTodo = ()=>{
-    setAllTodos([...allTodos,{todo}])
-  }
   return (
-    <Row gutter={[10,10]} className="App">
-      Todo app with TypeScript.
-      <InputField value={todo} placeholder='Enter your todo' setTodo={setTodo} />
-      <BasicButton addTodo={addTodo}/>
-      {
-        allTodos?.map(item => {
-          return<p>{item.todo}</p>
-        })
-      }
-      
+    <Row className="App">
+      <Col span={24}>
+        <TodoApp />
+      </Col>
     </Row>
   );
 };
---
+++ src/container/index.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState } from "react";
+import { InputField, BasicButton } from "../components";
+import { Row, Col,Avatar, Card, Skeleton, Switch } from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+import "antd/dist/antd.css";
+import "../App.css";
+
+const { Meta } = Card;
+const TodoApp: FC = () => {
+  interface Todos {
+    todo: string;
+  }
+  const [todo, setTodo] = useState<string>("");
+  const [allTodos, setAllTodos] = useState<Todos[]>([]);
+
+  const addTodo = () => {
+    setAllTodos([...allTodos, { todo }]);
+    setTodo("");
+  };
+  return (
+    <Row gutter={[10, 10]} className="App">
+      <Col span={24}>
+        <h2>Todo app with TypeScript.</h2>
+      </Col>
+      <Col span={24}>
+        <InputField
+          value={todo}
+          placeholder="Enter your todo"
+          setTodo={setTodo}
+        />
+      </Col>
+      <Col span={24}>
+        <BasicButton addTodo={addTodo} />
+      </Col>
+      <Col span={24}>
+
+     
+
+
+        {allTodos?.map((item) => {
+          return (
+            <Card
+            style={{ width: 300, marginTop: 16 }}
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            {/* <Skeleton loading={loading} avatar active> */}
+              <Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title="Card title"
+                description={item.todo}
+              />
+            {/* </Skeleton> */}
+          </Card>
+          );
+        })}
+      </Col>
+    </Row>
+  );
+};
+
+export default TodoApp;
